4d78b6ce117a2afbe2073aaa37e37bdb0a80c015
Valerie Tan <valerietan24@gmail.com>
2015-01-19 11:29:01 +0800

    UI changes and fix to time and date.
    Time still not based on timezone.

--- app/src/main/java/com/reminders/valerie/reminders/NewTaskActivity.java
+++ app/src/main/java/com/reminders/valerie/reminders/NewTaskActivity.java
@@ -11,6 +11,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.TimePicker;
+import android.widget.Toast;
 
 import java.text.DateFormatSymbols;
 import java.util.Calendar;
@@ -38,11 +39,11 @@ public class NewTaskActivity extends ActionBarActivity {
             task_month = monthOfYear;
             task_year = year;
             if(date_button_mgr != null) {
-                String date_text = date_button_mgr.buildButtonText(task_day, task_month, task_year);
+                String date_text = date_button_mgr.buildButtonText(task_year, task_month, task_day);
                 date_button_mgr.setButtonText(date_button, date_text);
             }
             else{
-                Log.d("null", "date button manager not found");
+                Toast.makeText(getApplicationContext(), "date_button_mgr is null", Toast.LENGTH_SHORT);
             }
         }
     };
@@ -58,42 +59,10 @@ public class NewTaskActivity extends ActionBarActivity {
                 time_button_mgr.setButtonText(time_button, time_text);
             }
             else{
-                Log.d("null", "time button manager not found");
+                Toast.makeText(getApplicationContext(), "time_button_mgr is null", Toast.LENGTH_SHORT);
             }
         }
     };
-
-    private void setDateButtonText() {
-        task_month_name = new DateFormatSymbols().getMonths()[task_month];
-        if(date_button != null){
-            date_button.setText( task_day + " " + task_month_name + " " + task_year);
-        }
-    }
-
-
-    private String buildTimeText(){
-        String time_text = "";
-        if(task_hour < 10){
-            time_text = time_text + "0" + task_hour;
-        }
-        else{
-            time_text = time_text + task_hour;
-        }
-        if(task_minute < 10){
-            time_text = time_text + ":0" + task_minute;
-        }
-        else{
-            time_text = time_text + ":" + task_minute;
-        }
-        return time_text;
-    }
-
-    private void setTimeButtonText() {
-        String time_text = buildTimeText();
-        if(time_button != null){
-            time_button.setText(time_text);
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState){
@@ -113,10 +82,11 @@ public class NewTaskActivity extends ActionBarActivity {
                 date_button_mgr.showPickerFragment(getSupportFragmentManager(), date_set_listener, date_args);
             }
         });
-        task_year = Calendar.getInstance().YEAR;
-        task_month = Calendar.getInstance().MONTH;
-        task_day = Calendar.getInstance().DAY_OF_MONTH;
-        date_button_mgr.setButtonText(date_button, date_button_mgr.buildButtonText(task_year, task_month, task_day));
+        task_year = Calendar.getInstance().get(Calendar.YEAR);
+        task_month = Calendar.getInstance().get(Calendar.MONTH);
+        task_day = Calendar.getInstance().get(Calendar.DAY_OF_MONTH);
+        String date_text = date_button_mgr.buildButtonText(task_year, task_month, task_day);
+        date_button_mgr.setButtonText(date_button, date_text);
 
         //time button
         time_button = (Button) findViewById(R.id.task_time_button);
@@ -130,25 +100,10 @@ public class NewTaskActivity extends ActionBarActivity {
                 time_button_mgr.showPickerFragment(getSupportFragmentManager(), time_set_listener, time_args);
             }
         });
-
-        //get current time
-        task_hour = Time.HOUR;
-        task_minute = Time.MINUTE;
-
+        task_hour = Calendar.getInstance().get(Calendar.HOUR_OF_DAY);
+        task_minute = Calendar.getInstance().get(Calendar.MINUTE);
         time_button_mgr.setButtonText(time_button, time_button_mgr.buildButtonText(task_hour, task_minute, 0));
 
-    }
-
-    private void showTimePicker(){
-        TimePickerDialogFragment time_picker = new TimePickerDialogFragment();
-        Bundle time_args = new Bundle();
-        time_args.putInt("task_hour", task_hour);
-        time_args.putInt("task_minute", task_minute);
-        time_picker.setArguments(time_args);
-        time_picker.setCallBack(time_set_listener);
-
-        FragmentManager fragment_mgr = getSupportFragmentManager();
-        time_picker.show(fragment_mgr, "dialog");
     }
 
 }
