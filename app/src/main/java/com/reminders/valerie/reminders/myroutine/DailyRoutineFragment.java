2787068020c9b7dae5da49a632d537965bb28c12
Valerie Tan <valerietan24@gmail.com>
2015-02-03 12:01:25 +0800

    added buttons in daily routine view.

--- app/src/main/java/com/reminders/valerie/reminders/myroutine/DailyRoutineFragment.java
+++ app/src/main/java/com/reminders/valerie/reminders/myroutine/DailyRoutineFragment.java
@@ -2,14 +2,19 @@ package com.reminders.valerie.reminders.myroutine;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.reminders.valerie.reminders.R;
 import com.reminders.valerie.reminders.model.PerDayEvent;
@@ -17,11 +22,14 @@ import com.reminders.valerie.reminders.model.PerDayEvent;
 import java.util.ArrayList;
 
 
-public class DailyRoutineFragment extends Fragment {
+public class DailyRoutineFragment extends Fragment implements View.OnClickListener{
     private String day;
     private TextView day_header;
     private ListView routine_listview;
     private DayRoutineAdapter list_adapter;
+
+    private ImageView add_icon, duplicate_icon;
+    private Button clear_all_button, save_button;
 
     public ArrayList<PerDayEvent> getRoutine_list() {
         return routine_list;
@@ -51,6 +59,40 @@ public class DailyRoutineFragment extends Fragment {
         routine_listview = (ListView) rootView.findViewById(R.id.daily_routine_list);
         list_adapter = new DayRoutineAdapter(getActivity(), routine_list);
         routine_listview.setAdapter(list_adapter);
+
+        add_icon = (ImageView) rootView.findViewById(R.id.add_icon);
+        add_icon.setClickable(true);
+        duplicate_icon = (ImageView) rootView.findViewById(R.id.duplicate_icon);
+        duplicate_icon.setClickable(true);
+
+        clear_all_button = (Button) rootView.findViewById(R.id.clear_all_button);
+        save_button = (Button) rootView.findViewById(R.id.save_button);
+
         return rootView;
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.add_icon:
+                //open add category dialog fragment
+                Toast.makeText(getActivity().getApplicationContext(), "adding category", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.duplicate_icon:
+                //open duplicate dialog fragment
+                Toast.makeText(getActivity().getApplicationContext(), "duplicating from other days", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.clear_all_button:
+                //open confirmation dialog
+                Toast.makeText(getActivity().getApplicationContext(), "Clearing all", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.save_button:
+                //save data and return
+                Toast.makeText(getActivity().getApplicationContext(), "data saved", Toast.LENGTH_SHORT).show();
+                FragmentManager fragment_mgr = getActivity().getSupportFragmentManager();
+                if(fragment_mgr.getBackStackEntryCount() > 0){
+                    fragment_mgr.popBackStack();
+                }
+        }
     }
 }
